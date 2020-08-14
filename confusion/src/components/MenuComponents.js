e2c5bd916cd86ee3da83fb843c1435eafa723b18
Sunidhi Sharma <47216094+sunidhisharma18@users.noreply.github.com>
2020-08-14 19:58:29 +0530

    Add files via upload

---
+++ confusion/src/components/MenuComponents.js
@@ -0,0 +1,58 @@
+import React ,{ Component } from 'react';
+import { Card, CardImg, CardImgOverlay,
+  CardTitle } from 'reactstrap';
+ 
+  class Menu extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      selectedDish: null
+  }
+  }
+
+  
+ /* renderDish(dish){
+    if(dish!=null){
+      return(
+        <Card>
+           <CardImg  width="100%" src={dish.image} alt={dish.name}>
+              </CardImg>
+              <CardBody>
+              < CardTitle>{dish.name}</ CardTitle>
+              <CardText>
+                {dish.description}
+              </CardText>
+              </CardBody>
+        </Card>
+      )
+
+    }else{
+      return(
+        <div></div>
+      )
+    }
+  }*/
+  render(){
+    const menu=this.props.dishes.map((dish)=>{
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card onClick={()=> this.props.onClick(dish.id)}>
+              <CardImg  width="100%" src={dish.image} alt={dish.name}>
+              </CardImg>
+              <CardImgOverlay>
+                < CardTitle>{dish.name}</ CardTitle>
+              </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+    return(
+      <div className="container">
+        <div className="row">
+            {menu}
+            </div>
+      </div>
+    );
+  }
+}
+export default Menu;
