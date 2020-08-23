e6739cf75931ca742199afe69c76f2e4d60ca6af
Sunidhi Sharma <47216094+sunidhisharma18@users.noreply.github.com>
2020-08-24 02:46:27 +0530

    Assginment-2 and controlled components

--- confusion/src/components/MenuComponents.js
+++ confusion/src/components/MenuComponents.js
@@ -1,58 +1,45 @@
-import React ,{ Component } from 'react';
+import React  from 'react';
 import { Card, CardImg, CardImgOverlay,
-  CardTitle } from 'reactstrap';
- 
-  class Menu extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      selectedDish: null
-  }
-  }
+  CardTitle , Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
-  
- /* renderDish(dish){
-    if(dish!=null){
-      return(
-        <Card>
-           <CardImg  width="100%" src={dish.image} alt={dish.name}>
-              </CardImg>
-              <CardBody>
-              < CardTitle>{dish.name}</ CardTitle>
-              <CardText>
-                {dish.description}
-              </CardText>
-              </CardBody>
-        </Card>
-      )
-
-    }else{
-      return(
-        <div></div>
-      )
-    }
-  }*/
-  render(){
-    const menu=this.props.dishes.map((dish)=>{
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={()=> this.props.onClick(dish.id)}>
-              <CardImg  width="100%" src={dish.image} alt={dish.name}>
-              </CardImg>
+function RenderMenuItem ({dish, onClick}) {
+  return (
+      <Card>
+          <Link to={`/menu/${dish.id}`} >
+              <CardImg width="100%" src={dish.image} alt={dish.name} />
               <CardImgOverlay>
-                < CardTitle>{dish.name}</ CardTitle>
+                  <CardTitle>{dish.name}</CardTitle>
               </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
-    return(
-      <div className="container">
-        <div className="row">
-            {menu}
-            </div>
+          </Link>
+      </Card>
+  );
+}
+const Menu = (props)=>{
+  const menu=props.dishes.map((dish)=>{
+    return (
+      <div key={dish.id} className="col-12 col-md-5 m-1">
+        <RenderMenuItem dish={dish} />
       </div>
     );
-  }
-}
+  });
+  return(
+    <div className="container">
+    <div className="row">
+        <Breadcrumb>
+            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+            <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+            <h3>Menu</h3>
+            <hr />
+        </div>                
+    </div>
+    <div className="row">
+        {menu}
+    </div>
+</div>
+  );
+  
+}    
 export default Menu;
